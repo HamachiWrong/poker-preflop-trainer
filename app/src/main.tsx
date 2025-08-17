f725c65901b3e3d76b2041e4fc0fcd71f26d9a36
Hamachi <kohei.arai@fred-japan.co.jp>
2025-08-17 17:34:37 +0900

    Update router to createHashRouter

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import UploadRanges from "./pages/UploadRanges";
 import Play from "./pages/Play";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   { path: "/", element: <UploadRanges /> },
   { path: "/play", element: <Play /> },
 ]);
